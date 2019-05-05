cd9b072ffb8bd28d06bba62ef5dcc0f057617c14
Ryan Spice-Finnie <spice.ryan@hotmail.com>
2019-05-05 11:59:26 -0400

    module name issue in prod, version

--- src/components/index.js
+++ src/components/index.js
@@ -1,16 +1,16 @@
 /*@flow*/
 
 let context:any = require.context(".", true, /\.js$/);
-let module:any = {};
+let _module:any = {};
 
 context.keys().forEach(function(key) {
 
 	let ctx:any = context(key).default;
 
 	for (let key in ctx) {
-		module[key.replace("./","").replace(".js","")] = ctx;
+		_module[key.replace("./","").replace(".js","")] = ctx;
 	}
 
 });
 
-export default module;
+export default _module;
