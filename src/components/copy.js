55323c0a415b2363af5fad909c6b2045eabd00d2
Ryan Spice-Finnie <spice.ryan@hotmail.com>
2019-06-02 21:30:10 -0400

    removing copy, styling css

--- src/components/copy.js
+++ src/components/copy.js
@@ -5,28 +5,28 @@ import {
 } from "../entry";
 
 class Copy extends AsyncView {
-	sequence: any = 6;
-	type: any = 'span';
-	style: any = `
-		font-family: monospace; text-align:center;
-		display:block !important;padding:2rem;display:block;min-width:320;max-width:780px;margin:0px auto;color:rgba(255,255,255,0.25);
-	`;
-	renderTo: any = `main`;
-	className: any = ``;
-	id: any = ``;
-	mounted: any = () => {
+
+	sequence: number = 6;
+	type: string = 'summary';
+	renderTo: string = `main`;
+	mounted: Function = function() {
 
 		feather.replace();
 
 	};
-	innerHTML: any = `
-Thanks - Merci - Grazi - Danken - Spasybi - 谢谢 - 감사
-<br/>
-<br/>
-<img style="width:40px" src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Flag_of_Canada_%28Pantone%29.svg/1920px-Flag_of_Canada_%28Pantone%29.svg.png"/>
-<br/>
-<br/>
-copyright &copy; ${new Date().getFullYear()} ryanspice.com`;
+	innerHTML: string = `
+		<p style="">
+		Visit <a href="https://github.com/ryanspice">https://github.com/ryanspice</a> or;<br/>
+		Contact me at <a href="mailto:spice.ryan@hotmail.com" target="_self">contact@ryanspice.com</a> to learn more about what I can do.<br/>
+		<p>
+		Thanks - Merci - Grazi - Danken - Spasybi - 谢谢 - 감사
+		<br/>
+		<br/>
+		<img  src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Flag_of_Canada_%28Pantone%29.svg/1920px-Flag_of_Canada_%28Pantone%29.svg.png"/>
+		<br/>
+		<br/>
+		copyright &copy; ${new Date().getFullYear()} ryanspice.com
+	`;
 }
 
-export default new Copy()
+export default new Copy();
