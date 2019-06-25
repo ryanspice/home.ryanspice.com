dea134b5e3bee0e3c6fb67b16608339d6c3a88fc
Ryan Spice-Finnie <spice.ryan@hotmail.com>
2019-06-24 21:23:48 -0400

    remove loader, and change name of ejs

--- src/index.js
+++ src/index.js
@@ -17,7 +17,8 @@ require("./components");
 AsyncTemplate.pre = async function(){
 
 	window['async-2018-mvc'].entry.sort((a,b)=>{return (a.sequence?a.sequence:0)-(b.sequence?b.sequence:0)});
-
+	if(document.getElementsByTagName('loader')[0])
+		document.getElementsByTagName('loader')[0].remove();
 };
 
 AsyncTemplate.post = () => {
