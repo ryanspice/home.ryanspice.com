cc7bd8bbcc32a7841e5fcf20e5a4eed47a4efb05
Ryan Spice-Finnie <spice.ryan@hotmail.com>
2019-07-05 11:42:58 -0400

    refactor SetColourTheme, filter empty colours

--- src/hexSorter.worker.js
+++ src/hexSorter.worker.js
@@ -9,7 +9,7 @@ onmessage = async function(e) {
 	console.log(e.data[1])
 	*/
 
-	const sorted = await hexSorter.sortColors(e.data[0],e.data[1]);
+	const sorted = await hexSorter.sortColors(e.data[0].filter(e=>(e.length>0)),e.data[1]);
 
 	postMessage({
 		primary:sorted[sorted.length-1],
